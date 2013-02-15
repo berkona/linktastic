c13cd726ea0736ae01963e814c6e6c475bbc6175
Berkona <berkona@gmail.com>
2013-02-14 23:51:45 -0500

    Added checking for errors and raising an OSError if we cannot

--- linktastic.py
+++ linktastic.py
@@ -2,10 +2,13 @@
 import subprocess
 import logging
 import os
-
+import re
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
+
+_hardlinkRegex = re.compile('Hardlink created for .+ <<==>> .+')
+_symlinkRegex = re.compile('symbolic link created for .+ <<==>> .+')
 
 
 # Private function to create link on nt-based systems
@@ -18,6 +21,11 @@ def _link_windows(src, dest):
 	logger.info(stdout)
 	logger.info(stderr)
 
+	if not _hardlinkRegex.match(stdout):
+		raise OSError(strerror=stdout)
+	elif not _hardlinkRegex.match(stderr):
+		raise OSError(strerror=stderr)
+
 
 def _symlink_windows(src, dest):
 	subprocess.call(['cmd', '/C', 'mklink', dest, src], stdout=subprocess.PIPE)
@@ -27,6 +35,11 @@ def _symlink_windows(src, dest):
 	# TODO, find out what kind of messages Windows sends us from mklink
 	logger.info(stdout)
 	logger.info(stderr)
+
+	if not _symlinkRegex.match(stdout):
+		raise OSError(strerror=stdout)
+	elif not _symlinkRegex.match(stderr):
+		raise OSError(strerror=stderr)
 
 
 # Create a hard link to src named as dest
